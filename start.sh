9485d9fc0d4e6f4090e0abe165ff1878722976be
Nejtr0n <a6y@xakep.ru>
2016-04-20 18:31:11 +0300

    Reload fix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-consul-template -consul=$CONSUL_URL -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:service haproxy reload"
+consul-template -consul=$CONSUL_URL -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:killall -HUP haproxy"
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
