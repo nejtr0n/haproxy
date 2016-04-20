5cdf371503b307ac12863a7bb234c41f94a6e43c
Nejtr0n <a6y@xakep.ru>
2016-04-20 18:43:05 +0300

    Chmod start script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 consul-template -consul=$CONSUL_URL -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:killall -HUP haproxy"
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
