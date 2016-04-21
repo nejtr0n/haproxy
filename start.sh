cefc754c9c5b158d1a3bd556b29a0aa9c548d4a4
nejtr0n <a6y@xakep.ru>
2016-04-21 14:28:16 +0300

    haproxy restart change

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-consul-template -consul=$CONSUL_URL -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:killall -HUP haproxy"
+consul-template -consul=$CONSUL_URL -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:haproxy -f /usr/local/etc/haproxy/haproxy.cfg -sf $(pidof haproxy) &"
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
