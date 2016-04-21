ba30e08758b13f0add0b01099e60370b34cb10b2
nejtr0n <a6y@xakep.ru>
2016-04-21 18:05:20 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-tail -f /dev/null
-#consul-template -consul=$CONSUL_URL -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:haproxy -f /usr/local/etc/haproxy/haproxy.cfg -sf $(pidof haproxy) &"
-#haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+consul-template -consul=$CONSUL_URL -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:haproxy -f /usr/local/etc/haproxy/haproxy.cfg -sf $(pidof haproxy) &"
+haproxy -f /usr/local/etc/haproxy/haproxy.cfg
