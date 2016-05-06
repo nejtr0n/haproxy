91471e67a59a861dc9854f142fd6c4ca478700e2
nejtr0n <a6y@xakep.ru>
2016-05-06 15:39:32 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-consul-template -consul=$CONSUL_URL -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:haproxy -f /usr/local/etc/haproxy/haproxy.cfg -sf $(pidof haproxy) &"
+consul-template -consul=$CONSUL_URL -wait "10s" -retry 10s -template="/templates/$HAPROXY_TEMPLATE:/usr/local/etc/haproxy/haproxy.cfg:kill -HUP $(pidof haproxy) || true" &
+sleep 15
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
